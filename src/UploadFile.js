a30e9ae741aabafb3c5725642336ae2c3eff95de
Aayushi <aayushi_shah@gridle.io>
2019-11-29 16:58:35 +0530

    Initial Setup

---
+++ src/UploadFile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UploadFile extends Component{
+    render(){
+        return <React.Fragment>
+            Hello World
+        </React.Fragment>
+    }
+}
+
+export default UploadFile
