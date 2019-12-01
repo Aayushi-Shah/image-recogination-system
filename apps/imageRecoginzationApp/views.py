97f3733fd547c4594bffe3cefafa964f646d2ee2
aayushi_shah <aayushi_shah@gridle.io>
2019-12-01 13:35:22 +0530

    [CHANGED]: google credential file

--- apps/imageRecoginzationApp/views.py
+++ apps/imageRecoginzationApp/views.py
@@ -19,7 +19,7 @@ class uploadFile(APIView):
     
     def post(self, request):
         #set google application credentials in environment variable 
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(settings.BASE_DIR, "Ymir-8feb90f29168.json")
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(settings.BASE_DIR, "Ymir-d8af5f8a3037.json")
         
         #get file from request
         file_data = request.FILES.get('document')
